ddf0e161e048d0fced22d9ea925b72e12fb31541
Jesrel <jesrelmalaqui@gmail.com>
2023-02-06 14:46:56 +0800

    tailwind

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php get_header(); ?>
 
 
-<h1>Hello World</h1>
-<h2>Hello World</h2>
+<h1 class="ml-10">Hello World</h1>
+<h2 class="font-extrabold text-lg">Hello World</h2>
 <h3>Hello World</h3>
 
 <?php get_footer(); ?>
