1e397e151bb271817abafa5bbfe9a180a814290c
Jesrel <jesrelmalaqui@gmail.com>
2023-02-07 13:52:35 +0800

    add header

--- components/header/header_login.php
+++ components/header/header_login.php
@@ -1,17 +1,14 @@
-<header class="bg-gray-900 p-4">
+<header class="bg-teal-800 p-4">
   <div class="container mx-auto">
-    <div class="flex items-center justify-between">
+    <div class="grid grid-flow-col justify-start items-center">
     <?php 
                 $image_id = 32; // replace with the actual image attachment ID 
                 $image_src = wp_get_attachment_image_src( $image_id, 'large' );
             ?>
 
             <img src="<?php echo esc_url( $image_src[0] ); ?>" alt="Virtualahan Logo" width="<?php echo esc_attr( $image_src[1] ); ?>" height="<?php echo esc_attr( $image_src[2] ); ?>">
-            <p class="text-teal-800">Virtual Eskwelahan</p>
-      <a href="<?php echo get_home_url(); ?>" class="font-bold text-white text-xl">My WordPress Site</a>
-      <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-        Login
-      </button>
+            <p class="text-white text-4xl">Virtual Eskwelahan</p>
+      
     </div>
   </div>
 </header>
