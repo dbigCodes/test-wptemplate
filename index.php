e163f0e0c89061c26888cdb438b78951e688afe6
Jesrel <jesrelmalaqui@gmail.com>
2023-02-07 14:27:36 +0800

    login

--- index.php
+++ index.php
@@ -4,9 +4,7 @@
 <div id="content" class="site-content">
 		<div id="primary" class="content-area">
 			<main id="main" class="site-main">
-<h1 class="ml-10">Hello World</h1>
-<h2 class="font-extrabold text-lg">Hello World</h2>
-<h3 class="text-teal-800">Hello World</h3>
+            <?php include( get_template_directory() . '../components/login/login.php' ); ?>
 </main>
 
 <?php get_footer(); ?>
